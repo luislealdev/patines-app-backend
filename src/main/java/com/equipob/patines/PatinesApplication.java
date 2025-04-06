55323fe49e34f71e53d0c0d05e770d97b8c21a32
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-04-06 16:21:51 -0600

    initialized project

---
+++ src/main/java/com/equipob/patines/PatinesApplication.java
@@ -0,0 +1,13 @@
+package com.equipob.patines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipob/patines/PatinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipob.patines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
